183571cdc1dac1777085bcde7d6ecd034674757b
Iryna <iryna.shulim@gmail.com>
2021-09-01 10:11:08 -0500

    changing comment count

--- models/Comment.js
+++ models/Comment.js
@@ -14,8 +14,22 @@ const CommentSchema = new Schema(
       default: Date.now,
       get: createdAtVal => dateFormat(createdAtVal)
     },
+    // use ReplySchema to validate data for a reply
     replies: [ReplySchema]
-  });
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+CommentSchema.virtual('replyCount').get(function() {
+  return this.replies.length;
+});
+
 
 const ReplySchema = new Schema(
   {
@@ -34,6 +48,11 @@ const ReplySchema = new Schema(
       type: Date,
       default: Date.now,
       get: createdAtVal => dateFormat(createdAtVal)
+    }
+  },
+  {
+    toJSON: {
+      getters: true
     }
   }
 );
